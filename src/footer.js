1d87b5182f8b0c67cf04de39a9267540950ea936
idungstanley <sundaystanley56@gmail.com>
2023-10-26 09:11:08 +0100

    added code for parent / children component

--- src/footer.js
+++ src/footer.js
@@ -1,12 +1,10 @@
 import React from 'react';
 
-
 function Footer(){
     return(
-        <footer>
-            <h3>The life </h3>
-        </footer>
+        <div>
+            <small>Copyright 2021</small>
+        </div>
     )
 }
-
 export default Footer;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-
-
-
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,5 +9,6 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
 
 
