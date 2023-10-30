289a350c15297ba570a1d06878d36926a98864fa
idungstanley <sundaystanley56@gmail.com>
2023-10-30 11:30:39 +0100

    added code for props react

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
 import React from 'react'
-import TodoList from './TodoList';
+import ContactCard from './ContactCard'
 
 function App() {
   return (
-    <div className='todo-list'>
-      <TodoList />
-      <TodoList />
-      <TodoList />
+    <div className="contacts">
+      <ContactCard
+        contact={{
+          name: 'Mr. Whiskerson',
+          imgUrl: 'http://placekitten.com/300/200',
+          phone: '(212) 555-1234',
+          email: 'mr.whiskaz@catnap.meow',
+        }}
+      />
+      <ContactCard
+        contact={{
+          imgUrl: 'http://placekitten.com/400/200',
+          name: 'Mr Fluffykins',
+          phone: '(212) 555-2345',
+          email: 'fluff@me.com',
+        }}
+      />
+      <ContactCard
+        contact={{
+          imgUrl: 'http://placekitten.com/400/300',
+          name: 'Mr Destroyer',
+          phone: '(212) 555-3456',
+          email: 'ofworlds@yahoo.com',
+        }}
+      />
+      <ContactCard
+        contact={{
+          imgUrl: 'http://placekitten.com/200/100',
+          name: 'Mr Felix',
+          phone: '(212) 555-4567',
+          email: 'thecat@hotmail.com',
+        }}
+      />
     </div>
   )
 }
 
 export default App
+
