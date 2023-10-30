2097903d5e9fcf9a89a2081bc23199f355ca2d2c
idungstanley <sundaystanley56@gmail.com>
2023-10-30 08:20:37 +0100

    added code for todo app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
+import React from 'react'
+import TodoList from './TodoList';
+
 function App() {
-  const date = new Date()
-  const hour = date.getHours()
-  let timeOfDay
-
-  if (hour < 12) {
-    timeOfDay = 'Morning'
-  } else if (hour >= 12 && hour < 17) {
-    timeOfDay = 'afternoon'
-  } else {
-    timeOfDay = 'Evening'
-  }
-
   return (
-    <div>
-      <h1 style={{ color: 'blue', background: 'red', padding: '20px'}}>good {timeOfDay} it's currently { hour % 12} O'clock</h1>
+    <div className='todo-list'>
+      <TodoList />
+      <TodoList />
+      <TodoList />
     </div>
   )
 }
---
+++ src/TodoList.js
@@ -0,0 +1,25 @@
+
+import React from 'react'
+
+
+function TodoList() {
+    return (
+        <div className="todo-item">
+            <input type="checkbox" />
+            <p>Placeholder text here</p>
+        </div>
+    );
+}
+
+export default TodoList;
+
+
+
+
+
+
+
+
+
+
+
